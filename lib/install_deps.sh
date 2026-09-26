9267f690e0f91886a09ef804acd9c882bc2bea08
Levon Gevorgyan <lgevorgyan03@gmail.com>
2026-09-26 16:19:27 -0500

    add patchelf for fedora

--- lib/install_deps.sh
+++ lib/install_deps.sh
@@ -78,7 +78,7 @@ print_req_pkgs() {
         llvm-cmake-utils llvm-devel
         llvm-static compiler-rt lld
         mkvtoolnix glib2-static
-        python3-devel
+        python3-devel patchelf
     )
     # shellcheck disable=SC2034
     local pkg_pkgs=(
